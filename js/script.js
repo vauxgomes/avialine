24d320c2902dd2f5b6fe3624fee4306049a4aafa
Vaux Gomes <vauxsandino@gmail.com>
2022-04-05 17:35:56 -0300

    toggler

--- js/script.js
+++ js/script.js
@@ -1,66 +1,86 @@
 window.onload = function () {
-    // PopupInfo
-    var popupInfo = document.querySelector('#popup-info')
+  var toggled = window.localStorage.getItem('sidebar-status')
 
-    document.querySelectorAll('.day.past').forEach((el) => {
-        el.addEventListener('click', (e) => {
-            var x = e.pageX
-            var y = e.pageY
+  if (toggled === null) {
+    toggled = false
+    window.localStorage.setItem('sidebar-status', JSON.stringify(toggled))
+  } else {
+    toggled = JSON.parse(toggled)
+  }
 
-            var pageWidth = document.querySelector('body').clientWidth
-            console.log(el.clientWidth)
+  if (toggled == true) {
+    document.querySelector('.sidebar').classList.add('toggled')
+  }
 
-            // Avoid horizontal overflow
-            if (pageWidth - x < 360) {
-                x -= 360 - (pageWidth - x)
-            }
+  var sideBarToggler = document.querySelector('.sidebar .toggler')
 
-            popupInfo.style.display = 'block'
-            popupInfo.style.left = `${x}px`
-            popupInfo.style.top = `${y}px`
-        })
-    })
+  sideBarToggler.addEventListener('click', () => {
+    toggled = document.querySelector('.sidebar').classList.toggle('toggled')
+    window.localStorage.setItem('sidebar-status', JSON.stringify(toggled))
+  })
 
-    document
-        .querySelector('#popup-info .btn-close')
-        .addEventListener('click', (e) => {
-            popupInfo.style.display = 'none'
-        })
+  // // PopupInfo
+  // var popupInfo = document.querySelector('#popup-info')
 
-    // PopupEditor
-    var popupEditor = document.querySelector('#popup-editor')
+  // document.querySelectorAll('.day.past').forEach((el) => {
+  //     el.addEventListener('click', (e) => {
+  //         var x = e.pageX
+  //         var y = e.pageY
 
-    document
-        .querySelectorAll('.day:not(.past)')
-        .forEach((el) => {
-            el.addEventListener('click', (e) => {
-                var x = e.pageX
-                var y = e.pageY
+  //         var pageWidth = document.querySelector('body').clientWidth
+  //         console.log(el.clientWidth)
 
-                var pageWidth = document.querySelector('body').clientWidth
+  //         // Avoid horizontal overflow
+  //         if (pageWidth - x < 360) {
+  //             x -= 360 - (pageWidth - x)
+  //         }
 
-                // Avoid horizontal overflow
-                if (pageWidth - x < 360) {
-                    x -= 360 - (pageWidth - x)
-                }
+  //         popupInfo.style.display = 'block'
+  //         popupInfo.style.left = `${x}px`
+  //         popupInfo.style.top = `${y}px`
+  //     })
+  // })
 
-                popupEditor.style.display = 'block'
-                popupEditor.style.left = `${x}px`
-                popupEditor.style.top = `${y}px`
-            })
-        })
+  // document
+  //     .querySelector('#popup-info .btn-close')
+  //     .addEventListener('click', (e) => {
+  //         popupInfo.style.display = 'none'
+  //     })
 
-    document.querySelector('#btn-close').addEventListener('click', (e) => {
-        popupEditor.style.display = 'none'
-    })
+  // // PopupEditor
+  // var popupEditor = document.querySelector('#popup-editor')
 
-    // QR-Code
-    document.querySelector('#activate-qr').addEventListener('click', (e) => {
-        document.querySelector('#qr-container').classList.add('active')
-        popupEditor.style.display = 'none'
-    })
+  // document
+  //     .querySelectorAll('.day:not(.past)')
+  //     .forEach((el) => {
+  //         el.addEventListener('click', (e) => {
+  //             var x = e.pageX
+  //             var y = e.pageY
 
-    document.querySelector('#qr-container').addEventListener('click', (e) => {
-        document.querySelector('#qr-container').classList.remove('active')
-    })
+  //             var pageWidth = document.querySelector('body').clientWidth
+
+  //             // Avoid horizontal overflow
+  //             if (pageWidth - x < 360) {
+  //                 x -= 360 - (pageWidth - x)
+  //             }
+
+  //             popupEditor.style.display = 'block'
+  //             popupEditor.style.left = `${x}px`
+  //             popupEditor.style.top = `${y}px`
+  //         })
+  //     })
+
+  // document.querySelector('#btn-close').addEventListener('click', (e) => {
+  //     popupEditor.style.display = 'none'
+  // })
+
+  // // QR-Code
+  // document.querySelector('#activate-qr').addEventListener('click', (e) => {
+  //     document.querySelector('#qr-container').classList.add('active')
+  //     popupEditor.style.display = 'none'
+  // })
+
+  // document.querySelector('#qr-container').addEventListener('click', (e) => {
+  //     document.querySelector('#qr-container').classList.remove('active')
+  // })
 }
